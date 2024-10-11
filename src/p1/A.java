d3df47aff403c055208e825600ae06bcaf1511ef
gk2work <gkt2work@gmail.com>
2024-10-12 00:29:49 +0530

    Java

---
+++ src/p1/A.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class A {
+	public static void main(String[]args) {
+		String s1= "   ";
+		
+		System.out.println(s1.isBlank());
+		System.out.println(s1.isEmpty());
+		System.out.println(s1.length());
+		
+	}
+}
